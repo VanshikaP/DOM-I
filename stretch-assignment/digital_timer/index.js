923bc4552aadd350ad02283b26f8f8e7a0a80182
VanshikaP <vanshikapundir92@gmail.com>
2020-02-17 14:54:36 -0800

    Timer completed

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -19,25 +19,34 @@ document.querySelector('body').prepend(createButton('start'));
 document.querySelector('body').append(createButton('stop'));
 document.querySelector('body').append(createButton('reset'));
 
+let secondTens = document.getElementById('secondTens');
+let secondOnes = document.getElementById('secondOnes');
+let msHundreds = document.getElementById('msHundreds');
+let msTens = document.getElementById('msTens');
+
+secondTens.textContent = 0;
+secondOnes.textContent = 0;
+msHundreds.textContent = 0;
+msTens.textContent = 0;
+
 document.querySelector('.start-button').addEventListener('click', function (){
   document.querySelector('.start-button').style.background = 'red';
-  let secondTens = document.getElementById('secondTens');
-  let secondOnes = document.getElementById('secondOnes');
-  let msHundreds = document.getElementById('msHundreds');
-  let msTens = document.getElementById('msTens');
+  document.querySelector('.start-button').disabled = 'true';
 
-  secondTens.textContent = 0;
-  secondOnes.textContent = 0;
-  msHundreds.textContent = 0;
-  msTens.textContent = 0;
-  
+  document.querySelector('.stop-button').style.background = 'teal';
+  document.querySelector('.reset-button').style.background = 'teal';
+
   var timerSeconds = setInterval(seconds, 1000);
   
   function seconds(){
+    
     if(secondOnes.textContent == 9 && secondTens.textContent == 0){
       secondOnes.textContent = 0;
-      secondTens.textContent = 1
-      clearInterval(timerSecondOnes);
+      secondTens.textContent = 1;
+      clearInterval(timerSeconds);
+    } else if(secondTens.textContent == 1){
+      document.querySelector('.start-button').disabled = false;
+      clearInterval(timerSeconds);
     } else {
       secondOnes.textContent++;
     }
@@ -46,30 +55,37 @@ document.querySelector('.start-button').addEventListener('click', function (){
   var timerms = setInterval(milliseconds, 10);
   
   function milliseconds(){
-    if(msTens.textContent == 9 && msHundreds.textContent != 9){
-      msTens.textContent = 0;
-      msHundreds.textContent++;
-    } else if(msTens.textContent ==9 && msHundreds.textContent == 9){
-      msTens.textContent = 0;
+    if(secondOnes.textContent == 0 && secondTens.textContent == 1){
       msHundreds.textContent = 0;
-      if(secondOnes.textContent != 9){
-        milliseconds();
-      } else {
-        clearInterval(timerms);
-      }
+      msTens.textContent = 0;
+      document.querySelector('.start-button').style.background = 'teal';
+      clearInterval(timerms);
     } else {
-      msTens.textContent++;
+      if(msTens.textContent == 9 && msHundreds.textContent != 9){
+        msTens.textContent = 0;
+        msHundreds.textContent++;
+      } else if(msTens.textContent == 9 && msHundreds.textContent == 9){
+        msTens.textContent = 0;
+        msHundreds.textContent = 0;
+      } else {
+        msTens.textContent++;
+      }
     }
+    
   }
 
   
   document.querySelector('.stop-button').addEventListener('click', function (){
     document.querySelector('.stop-button').style.background = 'blue';
+    document.querySelector('.start-button').disabled = false;
+    document.querySelector('.start-button').style.background = 'teal';
     clearInterval(timerSeconds);
     clearInterval(timerms);
   })
   document.querySelector('.reset-button').addEventListener('click', () => {
       document.querySelector('.reset-button').style.background = 'green';
+      document.querySelector('.start-button').disabled = false;
+      document.querySelector('.start-button').style.background = 'teal';
       clearInterval(timerSeconds);
       clearInterval(timerms);
       secondOnes.textContent = 0;
